2a4a2184149e80585e3da8ebca8f6496de31005d
ruchik-panwal <ruchik.panwal@gmail.com>
2025-05-31 18:34:36 +0530

    made a function whic returns a lsit for infoElements to create them dynamically

---
+++ src/modules/extraInfoList.js
@@ -0,0 +1,30 @@
+let infoList = [
+  {
+    iconName: "thermostat",
+    title: "Feels Like"
+  },
+  {
+    iconName: "water_drop",
+    title: "Humidity"
+  },
+  {
+    iconName: "air",
+    title: "Wind"
+  },
+  {
+    iconName: "cloud",
+    title: "Chance of Rain"
+  },
+  {
+    iconName: "masks",
+    title: "Air Quality"
+  },
+  {
+    iconName: "lens_blur",
+    title: "Dew"
+  },
+];
+
+export default function extraInfoList(){
+    return infoList;
+}
