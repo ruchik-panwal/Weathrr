acf3a2326ee21a3f32b85627031db6899b97998c
ruchik-panwal <ruchik.panwal@gmail.com>
2025-05-31 16:14:39 +0530

    pushing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import weatherApi from "./modules/apiCaller";
+import mainDisplayDom from "./modules/domCreaator";
 
+import "/src/styles/main.css";
+
+// Temporary API Stuff
 const API_KEY = "EXCNRQ7ZZ6XGB5KGN7HGNHGWT";
 const VC_URL =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
@@ -7,3 +11,4 @@ const place = "nerul";
 const unit = "uk";
 
 weatherApi(VC_URL, API_KEY, place, unit);
+mainDisplayDom();
