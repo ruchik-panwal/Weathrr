258ca52c22cb11858478d13d2fb3da0f2ec9343e
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-02 02:47:11 +0530

    setting up buttons

---
+++ src/modules/buttonControl.js
@@ -0,0 +1,36 @@
+import { getUnit, setUnit, setPlace, divList } from "./extraInfoList";
+import infoDisplay from "./infoDisplay";
+
+function searchPlace() {
+  const searchBtn = document.querySelector(".searchBtn");
+  const inp = document.querySelector(".searchbar");
+
+  searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    setPlace(inp.value);
+    infoDisplay();
+  });
+}
+
+function toggleUnit() {
+  const togBtn = document.querySelector(".toggleUnit");
+  togBtn.addEventListener("click", () => {
+    getUnit() == "us" ? setUnit("uk") : setUnit("us");
+    domUnitChanger();
+    infoDisplay();
+  });
+}
+
+function domUnitChanger() {
+  divList().forEach((data) => {
+    if (getUnit() == "us") {
+      data.unit == "kph" ? (data.unit = "mph") : null;
+      data.unit == "°C" ? (data.unit = "°F") : null;
+    } else {
+      data.unit == "mph" ? (data.unit = "kph") : null;
+      data.unit == "°F" && data.name ? (data.unit = "°C") : null;
+    }
+  });
+}
+
+export { searchPlace, toggleUnit };
