d16957f081e9061e6df546138114620f0964518a
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-02 02:48:46 +0530

    pushing

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import {
   mainDisplayDom,
   extraInfoDom,
   futureForecastDom,
+  header,
 } from "./modules/domCreator";
 
+import { searchPlace , toggleUnit} from "./modules/buttonControl";
 import infoDisplay from "./modules/infoDisplay";
 
 import "/src/styles/fontsColors.css";
 import "/src/styles/main.css";
 
+header();
+searchPlace();
+toggleUnit();
 mainDisplayDom();
 extraInfoDom();
 futureForecastDom();
-
 infoDisplay();
