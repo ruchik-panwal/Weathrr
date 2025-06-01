69139835c5a44a53b7165e7f7fd4ac34b765b55e
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-02 01:34:02 +0530

    added new data

--- src/modules/extraInfoList.js
+++ src/modules/extraInfoList.js
@@ -1,30 +1,53 @@
 let infoList = [
   {
     iconName: "thermostat",
-    title: "Feels Like"
+    title: "Feels Like",
+    classname: "feelslike",
   },
   {
     iconName: "water_drop",
-    title: "Humidity"
+    title: "Humidity",
+    classname: "humidity",
   },
   {
     iconName: "air",
-    title: "Wind"
+    title: "Wind",
+    classname: "windspeed",
   },
   {
     iconName: "cloud",
-    title: "Chance of Rain"
+    title: "Chance of Rain",
+    classname: "precipprob",
   },
   {
     iconName: "flare",
-    title: "Solar Radiation"
+    title: "Solar Radiation",
+    classname: "solarradiation",
   },
   {
     iconName: "lens_blur",
-    title: "Dew"
+    title: "Dew",
+    classname: "dew",
   },
 ];
 
-export default function extraInfoList(){
-    return infoList;
+let data = [
+  { name: "resolvedAddress", parentobj: "", unit: "" },
+  { name: "description", parentobj: "", unit: "" },
+  { name: "temp", parentobj: "currentConditions", unit: "°C" },
+  { name: "feelslike", parentobj: "currentConditions", unit: "°C" },
+  { name: "conditions", parentobj: "currentConditions", unit: "" },
+  { name: "humidity", parentobj: "currentConditions", unit: "%" },
+  { name: "windspeed", parentobj: "currentConditions", unit: "mph" },
+  { name: "precipprob", parentobj: "currentConditions", unit: "%" },
+  { name: "solarradiation", parentobj: "currentConditions", unit: "W/m²" },
+  { name: "dew", parentobj: "currentConditions", unit: "°C" }
+];
+
+export function extraInfoList() {
+  return infoList;
+}
+
+export function divList() {
+  return data;
 }
